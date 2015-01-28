43b752abd929496da39afae0001c5d564fd311f7
kosmodrey <kosmodrey@yandex.ru>
2015-01-28 14:59:52 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# firefoxos-profile-bat
+
+Backup and restore your Firefox OS profile (contacts, SMS, Wi-Fi, settings and apps).
+
+### Usage:
+
+```
+profile.bat <backup|restore> [backup_name]
+```
+
+By default ```[backup_name]``` folder will be named as current date in ```DD-MM-YYYY_HHMMSS``` format. All backups will be stored in local ```/backup``` directory, but you can freely change ```BackupRoot``` and ```BackupPath``` path keys.
+
+### Make a backup:
+
+```
+> profile.bat backup my_named_backup
+
+Backup profile...
+Creating "my_named_backup" backup...
+
+List of devices attached
+36fec263        device
+
++ Stop b2g...
++ Backup Wi-Fi config...
++ Backup user profile folder...
++ Backup user local data...
++ Start b2g...
++ Backup done!
+
+Check D:\Backup\Flame\my_named_backup\backup-log.txt for details.
+```
+
+### Restore from backup:
+
+```
+> profile.bat restore my_named_backup
+
+Restore profile...
+Do you really want restore "my_named_backup" backup? (Y/N) Y
+
+List of devices attached
+36fec263        device
+
++ Stop b2g...
++ Remove old user profile...
++ Restore Wi-Fi config...
++ Restore user profile...
++ Restore user local data...
++ Start b2g...
++ Restore done!
+
+Check D:\Backup\Flame\my_named_backup\restore-log.txt for details.
+```
+
+### How about Linux and Mac OS?
+
+Check out: https://github.com/Mozilla-TWQA/B2G-flash-tool/blob/master/backup_restore_profile.sh
+
+### Requirements:
+
+- Windows 2000 and above (XP, Vista, 7, 8 or 10)
+- ADB (Android Debug Bridge) tool
---
+++ profile.bat
@@ -0,0 +1,122 @@
+::
+:: profile.bat
+:: Restore and backup Firefox OS profile.
+:: Usage: profile.bat <backup|restore> [backup_name]
+::
+:: https://github.com/kosmodrey/firefoxos-profile-bat
+::
+
+@echo off
+
+cls
+
+setlocal EnableExtensions
+
+set BackupTime=%time%
+set BackupDate=%date%
+set BackupRoot=%~dp0
+set BackupPath=%BackupRoot%backup
+set BackupHours=%BackupTime:~0,2%
+if "%BackupTime:~0,1%"==" " set BackupHours=0%BackupTime:~1,1%
+set BackupProfileFolder=%BackupDate:~7,2%-%BackupDate:~-10,2%-%BackupDate:~-4,4%_%BackupHours%%BackupTime:~3,2%%BackupTime:~6,2%
+set BackupLog=backup-log.txt
+set BackupRestoreLog=restore-log.txt
+
+set arg_do=%1
+set arg_backup=%2
+
+set run_adb=adb
+
+if "%arg_do%"=="backup" goto DO_BACKUP
+if "%arg_do%"=="restore" goto DO_RESTORE
+echo Invalid arguments. & goto DO_INFO
+goto:eof
+
+:DO_INFO
+
+	echo.Usage: profile.bat ^<backup^|restore^> [backup_name]
+	exit /b
+
+:goto:eof
+
+:DO_BACKUP
+
+	echo Backup profile...
+
+	if not exist "%BackupPath%" mkdir %BackupPath%
+	cd %BackupPath%
+
+	if not "%arg_backup%"=="" set BackupProfileFolder=%arg_backup%
+
+	echo Creating "%BackupProfileFolder%" backup...
+	mkdir %BackupProfileFolder%
+	cd %BackupProfileFolder%
+
+	echo.
+	call %run_adb% devices
+
+	echo + Stop b2g...
+	call %run_adb% shell stop b2g >> %BackupLog% 2>&1
+
+	echo + Backup Wi-Fi config...
+	call %run_adb% pull /data/misc/wifi/wpa_supplicant.conf ./wifi/wpa_supplicant.conf >> %BackupLog% 2>&1
+
+	echo + Backup user profile folder...
+	mkdir profile
+	call %run_adb% pull /data/b2g/mozilla ./profile >> %BackupLog% 2>&1
+
+	echo + Backup user local data...
+	mkdir local
+	call %run_adb% pull /data/local ./local >> %BackupLog% 2>&1
+
+	echo + Start b2g...
+	call %run_adb% shell start b2g >> %BackupLog% 2>&1
+
+	echo + Backup done!
+	echo.
+	echo Check %BackupPath%\%BackupProfileFolder%\%BackupLog% for details.
+
+goto:eof
+
+:DO_RESTORE
+
+	echo Restore profile...
+
+	if "%arg_backup%"=="" echo ! Missing backup name argument. & goto DO_INFO
+	if not exist "%BackupPath%\%arg_backup%" echo ! Backup folder "%BackupPath%\%arg_backup%" does't exist. & echo.Abort. & exit /b
+	if not exist "%BackupPath%\%arg_backup%\wifi" echo ! Missing "%BackupPath%\%arg_backup%\wifi" folder. & echo.Abort. & exit /b
+	if not exist "%BackupPath%\%arg_backup%\local" echo ! Missing "%BackupPath%\%arg_backup%\local" folder. & echo.Abort. & exit /b
+	if not exist "%BackupPath%\%arg_backup%\profile" echo ! Missing "%BackupPath%\%arg_backup%\profile" folder. & echo.Abort. & exit /b
+
+	cd %BackupPath%\%arg_backup%
+
+	set /p confirm=Do you really want restore "%arg_backup%" backup? ^(Y/N^) 
+	if not %confirm%==Y echo ! Operation canceled by user. & exit /b
+
+	echo.
+	call %run_adb% devices
+
+	echo + Stop b2g...
+	call %run_adb% shell stop b2g >> %BackupRestoreLog% 2>&1
+
+	echo + Remove old user profile...
+	call %run_adb% shell rm -r /data/b2g/mozilla >> %BackupRestoreLog% 2>&1
+
+	echo + Restore Wi-Fi config...
+	call %run_adb% push ./wifi /data/misc/wifi >> %BackupRestoreLog% 2>&1
+	call %run_adb% shell chown wifi.wifi /data/misc/wifi/wpa_supplicant.conf >> %BackupRestoreLog% 2>&1
+
+	echo + Restore user profile...
+	call %run_adb% push ./profile /data/b2g/mozilla >> %BackupRestoreLog% 2>&1
+
+	echo + Restore user local data...
+	call %run_adb% push ./local /data/local >> %BackupRestoreLog% 2>&1
+
+	echo + Start b2g...
+	call %run_adb% shell start b2g >> %BackupRestoreLog% 2>&1
+
+	echo + Restore done!
+	echo.
+	echo Check %BackupPath%\%BackupProfileFolder%\%BackupRestoreLog% for details.
+
+goto:eof
